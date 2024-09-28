5c8a5d63a5d6d04312fac838bc881f470acc4a70
thiag_oJorge <thiagojorgedasilva7@gmail.com>
2024-09-27 21:51:29 -0300

    version 0.0.6

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation'
 const Navbar = () => {
     const router = useRouter()
     const handleClick = () => {
-        return router.push('/login')
+        return router.push('/profile')
     }
     const goToFeed = () => {
         return router.push('/')
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     return (
         <div className='z-50 px-3 items-center justify-between flex fixed bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 drop-shadow-2xl text-white font-extrabold text-xl h-14 w-full'>
-            <button onclick={goToFeed} className=''>[Social Posting]</button>
+            <button onClick={goToFeed} className=''>[Social Posting]</button>
             <div className='rounded-full'>
             <label className='mt-2 w-4/5 md:w-96 h-10 rounded-full focus:drop-shadow-md focus:outline-none text-xl border-2 px-4 bg-white'>
                   <input 
@@ -26,11 +26,11 @@ const Navbar = () => {
             </label>
             </div> 
             <div className='flex items-center'>
-                <button onClick={handleClick} className='rounded-full border-black border-2 w-12 h-12'>
-                        <img alt="Profile" />
-                </button>
-                <button className='rounded-full border-black border-2 w-12 h-12 ml-3'>
+                <button className='rounded-full w-12 h-12 ml-3'>
                     <DarkModeIcon />
+                </button>
+                <button onClick={handleClick} className='rounded-full w-12 h-12 overflow-hidden'>
+                    <img src='perfil.jpg' alt="Profile" className='object-cover w-full h-full' />
                 </button>
             </div>
         </div>)
